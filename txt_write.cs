cea0bd2e3819e36f2178240ef69064ec38851ba9
juanhlb <juanhlb@github.com>
2019-03-27 21:09:30 -0400

    Create txt_write.cs

---
+++ txt_write.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace write_txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt_input = "";
+            txt_input= Console.ReadLine();
+
+            StreamWriter ofile;
+            ofile = new StreamWriter("prueba.txt");
+
+            ofile.WriteLine(txt_input);
+            ofile.Close();
+
+            Console.WriteLine("file written");
+          
+        }
+    }
+}
